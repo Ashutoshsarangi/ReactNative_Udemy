1c1aca2bd1c678f147a95ab247bc755ddd113bab
Ashu <ashutosh.softronet@gmail.com>
2020-05-02 03:22:09 +0530

    Added Styling and Wrapping up this Demo 1 Application.

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -3,13 +3,48 @@ import { View, StyleSheet, Text } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen Works</Text>
+    <View style={styles.viewStyle}>
+      <View style={styles.viewOneStyle}></View>
+      <View style={styles.viewTwoStyle}></View>
+      <View style={styles.viewThreeStyle}></View>
     </View>
   );
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    // borderWidth: 3,
+    // borderColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  viewOneStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  },
+  viewTwoStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    width: 100,
+    height: 100,
+    // top: 100,
+    // position: 'absolute',
+    // top: 100,
+    // left: 150,
+    marginTop: 100,
+    backgroundColor: 'green'
+  },
+  viewThreeStyle: {
+    borderWidth: 1,
+    borderColor: 'red',
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue'
+  }
+});
 
 export default BoxScreen;
